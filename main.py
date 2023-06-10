4f93f215e6013d78d7c19cf9735f7fbbe15aebfe
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-06-10 12:17:49 +0800

    add starter code

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+import csv
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/game')
+def game():
+    with open('cards.json') as f:
+        cards = json.load(f)
+    return render_template('memoryCards.html', cards=cards)
+
+@app.route('/save', methods=['POST'])
+def save():
+    data = request.get_json()
+
+    with open('leaderboard.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([data['name'], data['adminNo'], data['time']])
+
+    return '', 204  # No content
+
+if __name__ == '__main__':
+    app.run(debug=True)
