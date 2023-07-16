91f5c318d279ef97026afdda675489b78e742cac
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-07-16 22:30:51 +0800

    update: filter option

--- static/homepage.js
+++ static/homepage.js
@@ -10,18 +10,6 @@ const toggle = () => {
   document.body.classList.toggle("toggled");
 }
 
-const handleOnClick = index => {
-  toggle();
-  
-  anime({
-    targets: ".tile",
-    opacity: toggled ? 0 : 1,
-    delay: anime.stagger(50, {
-      grid: [columns, rows],
-      from: index
-    })
-  });
-}
 
 const createTile = index => {
   const tile = document.createElement("div");
@@ -30,7 +18,6 @@ const createTile = index => {
   
   tile.style.opacity = toggled ? 0 : 1;
   
-  tile.onclick = e => handleOnClick(index);
   
   return tile;
 }
@@ -58,3 +45,29 @@ const createGrid = () => {
 createGrid();
 
 window.onresize = () => createGrid();
+
+// Get references to the input element and the table body
+var input = document.getElementById('username');
+var table = document.getElementById('leaderboard');
+
+// Add an event listener to the input element
+input.addEventListener('keyup', function() {
+    // Get the current value of the input
+    var filter = input.value.toUpperCase();
+
+    // Get all the rows in the table
+    var rows = table.querySelectorAll('tr[data-username]');
+
+    // Loop through each row
+    for (var i = 0; i < rows.length; i++) {
+        // Get the username for this row
+        var username = rows[i].getAttribute('data-username');
+
+        // If the username includes the filter, show the row, otherwise hide it
+        if (username.toUpperCase().includes(filter)) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+});
